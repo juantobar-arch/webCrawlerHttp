0b26f7658adaff806dd0407f61b41b0dc15ab878
Tobar <juan.tobar@globant.com>
2025-12-09 18:16:01 -0500

    Implement page crawling functionality and update main script for argument handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,30 @@
 const { JSDOM } = require('jsdom');
 
+async function crawlPage(currentURL) {
+    console.log(`Currently crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL);
+
+        if (resp.status > 399) {
+            console.log(`error in fetch: ${resp.status} on page: ${currentURL} `);
+            return
+        }
+
+        const contentType = resp.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`);
+            return
+        }
+
+        console.log( await resp.text())
+    } catch (err) {
+        console.log(`error in fetch: ${err.message}, on page: ${currentURL} `);
+    }
+
+    
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -14,6 +39,7 @@ function getURLsFromHTML(htmlBody, baseURL) {
                 console.log(`error with relative URL: ${err.message}`);
             }
         }else {
+            // absolute URL
             try {
                 const urlObj = new URL(linkElement.href);
                 urls.push(urlObj.href);
@@ -38,4 +64,5 @@ function normalizeURL(urlString){
 module.exports = {
     normalizeURL,
     getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("Hello, World!");
+const { crawlPage } = require('./crawl.js');
+
+function main() {
+    if (process.argv.length < 3) {
+        console.log("No webside provided.");
+        process.exit(1);
+    }
+    if (process.argv.length > 3) {
+        console.log("Too many arguments provided.");
+        process.exit(1);
+    }
+    const baseURL = process.argv[2];
+    crawlPage(baseURL);
+    
+
+    console.log(`Starting crawl of ${baseURL}`)
+}
+
+main();
+
+// The first argument of the program is always the name of the program itself.
+// The second argument is the name of our code or entry point file.
+// The third argument is the one we provide when executing the program.
